7d61bd88b9e29c3a1840300f041a0640bf5bec1e
Arunchhibber <arunchhibber0101@gmail.com>
2025-12-08 15:39:45 -0500

    getting the home.js error when runing the server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/upload" element={<Upload />} />
