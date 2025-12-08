76deab2700a71697084c01ba5010f9bd0e84138a
Arunchhibber <arunchhibber0101@gmail.com>
2025-12-07 23:49:14 -0500

    created the app.js file and tested the server and its working fine

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Upload from "./components/Upload";
+import MyFiles from "./components/MyFiles";
+import Downloads from "./components/Downloads";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/my-files" element={<MyFiles />} />
+        <Route path="/downloads" element={<Downloads />} />
+      </Routes>
+    </Router>
   );
 }
 
